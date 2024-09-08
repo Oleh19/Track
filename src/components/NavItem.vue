63025100e728676e0960e3aefe9a21e65b627c3d
Ilchenko Oleh <deckart212@gmail.com>
2024-09-08 16:43:55 +0300

    Dev  active page highlighting

--- src/components/NavItem.vue
+++ src/components/NavItem.vue
@@ -1,16 +1,7 @@
 <template>
   <li class="flex-1">
-    <a :class="$attrs" class="flex flex-col items-center p-2 text-xs capitalize">
+    <a class="flex flex-col items-center p-2 text-xs capitalize cursor-pointer">
       <slot></slot>
-      {{ page }}
     </a>
   </li>
 </template>
-
-<script setup></script>
-
-<script>
-export default {
-  inheritAttrs: false
-}
-</script>
--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,8 +1,15 @@
 <template>
   <nav class="sticky bottom-0 z-10 bg-white">
     <ul class="flex items-center justify-around border-t">
-      <NavItem v-for="page in navItems" :key="page" :href="`#${page}`">
+      <NavItem
+        v-for="page in navItems"
+        :key="page"
+        :href="`#${page}`"
+        :class="{ 'bg-gray-200 pointer-events-none': page === currentPage }"
+        @click="currentPage = page"
+      >
         <component :is="getIconComponent(page)" class="w-6 h-6" />
+        {{ page }}
       </NavItem>
     </ul>
   </nav>
@@ -12,6 +19,7 @@
 import NavItem from './NavItem.vue'
 
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
+import { ref } from 'vue'
 
 const navItems = ['timeline', 'activities', 'progress']
 
@@ -21,5 +29,6 @@ const icons = {
   progress: ChartBarIcon
 }
 
+const currentPage = ref('timeline')
 const getIconComponent = (page) => icons[page]
 </script>
