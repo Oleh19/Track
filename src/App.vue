87c3841644363b06868302349ffd5aa09ffa1e60
Ilchenko Oleh <deckart212@gmail.com>
2024-09-08 15:32:09 +0300

    Dev navigation

--- src/App.vue
+++ src/App.vue
@@ -18,9 +18,32 @@
     </a>
   </header>
 
-  
+  <main class="flex flex-grow flex-col"></main>
+
+  <nav class="sticky bottom-0 z-10 bg-white">
+    <ul class="flex items-center justify-around border-t">
+      <li class="flex-1">
+        <a class="flex flex-col items-center p-2 text-xs capitalize" href="#timeline"
+          ><ClockIcon class="w-6 h-6" /> Timeline
+        </a>
+      </li>
+
+      <li class="flex-1">
+        <a class="flex flex-col items-center p-2 text-xs capitalize" href="#activities">
+          <ListBulletIcon class="w-6 h-6" /> Activities</a
+        >
+      </li>
+
+      <li class="flex-1">
+        <a class="flex flex-col items-center p-2 text-xs capitalize" href="#progress">
+          <ChartBarIcon class="w-6 h-6" />Progress
+        </a>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <script setup>
 import { CheckCircleIcon } from '@heroicons/vue/24/solid'
+import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
 </script>
