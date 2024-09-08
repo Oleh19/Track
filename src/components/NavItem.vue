c9bd732efc20a77cd9ec6fc3ee9106883540a69e
Ilchenko Oleh <deckart212@gmail.com>
2024-09-08 16:57:28 +0300

    Normalize page hash

--- src/components/NavItem.vue
+++ src/components/NavItem.vue
@@ -1,7 +1,13 @@
 <template>
   <li class="flex-1">
-    <a class="flex flex-col items-center p-2 text-xs capitalize cursor-pointer">
+    <a v-bind="$attrs" class="flex flex-col items-center p-2 text-xs capitalize">
       <slot></slot>
     </a>
   </li>
 </template>
+
+<script>
+export default {
+  inheritAttrs: false
+}
+</script>
--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -2,33 +2,41 @@
   <nav class="sticky bottom-0 z-10 bg-white">
     <ul class="flex items-center justify-around border-t">
       <NavItem
-        v-for="page in navItems"
+        v-for="(icon, page) in navItems"
         :key="page"
         :href="`#${page}`"
-        :class="{ 'bg-gray-200 pointer-events-none': page === currentPage }"
+        :class="{ 'pointer-events-none bg-gray-200': page === currentPage }"
         @click="currentPage = page"
       >
-        <component :is="getIconComponent(page)" class="w-6 h-6" />
-        {{ page }}
+        <component :is="icon" class="h-6 w-6" /> {{ page }}
       </NavItem>
     </ul>
   </nav>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
+import { PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS } from '../constants'
 import NavItem from './NavItem.vue'
 
-import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
-import { ref } from 'vue'
-
-const navItems = ['timeline', 'activities', 'progress']
-
-const icons = {
-  timeline: ClockIcon,
-  activities: ListBulletIcon,
-  progress: ChartBarIcon
+const navItems = {
+  [PAGE_TIMELINE]: ClockIcon,
+  [PAGE_ACTIVITIES]: ListBulletIcon,
+  [PAGE_PROGRESS]: ChartBarIcon
 }
 
-const currentPage = ref('timeline')
-const getIconComponent = (page) => icons[page]
+const currentPage = ref(normalizePageHash())
+
+function normalizePageHash() {
+  const hash = window.location.hash.slice(1)
+
+  if (Object.keys(navItems).includes(hash)) {
+    return hash
+  }
+
+  window.location.hash = PAGE_TIMELINE
+
+  return PAGE_TIMELINE
+}
 </script>
