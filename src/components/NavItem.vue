510991418c9c0bb494c8bfa3c50fc9a43b3de77e
Ilchenko Oleh <deckart212@gmail.com>
2024-09-08 16:24:29 +0300

    Refactoring

---
+++ src/components/NavItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <li class="flex-1">
+    <a :class="$attrs" class="flex flex-col items-center p-2 text-xs capitalize">
+      <slot></slot>
+      {{ page }}
+    </a>
+  </li>
+</template>
+
+<script setup></script>
+
+<script>
+export default {
+  inheritAttrs: false
+}
+</script>
--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,17 +1,16 @@
 <template>
   <nav class="sticky bottom-0 z-10 bg-white">
     <ul class="flex items-center justify-around border-t">
-      <li v-for="page in navItems" :key="page" class="flex-1">
-        <a :href="`#${page}`" class="flex flex-col items-center p-2 text-xs capitalize">
-          <component :is="getIconComponent(page)" class="w-6 h-6" />
-          {{ page }}
-        </a>
-      </li>
+      <NavItem v-for="page in navItems" :key="page" :href="`#${page}`">
+        <component :is="getIconComponent(page)" class="w-6 h-6" />
+      </NavItem>
     </ul>
   </nav>
 </template>
 
 <script setup>
+import NavItem from './NavItem.vue'
+
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
 
 const navItems = ['timeline', 'activities', 'progress']
