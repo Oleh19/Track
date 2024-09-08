cf094e47d0ef3cc2c3a17628dff182e9b4ad2ffb
Ilchenko Oleh <deckart212@gmail.com>
2024-09-09 00:21:21 +0300

    feat/timeline: add constants, functions, and timeline components
     - added constants and functions files
    
     - implemented timeline hours generation
    
     - added TimelineItem component for timeline elements

--- src/App.vue
+++ src/App.vue
@@ -11,27 +11,17 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import { PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS } from './constants'
 import TheHeader from './components/TheHeader.vue'
 import TheNav from './components/TheNav.vue'
 import TheTimeline from './pages/TheTimeline.vue'
 import TheActivities from './pages/TheActivities.vue'
 import TheProgress from './pages/TheProgress.vue'
 
+import { ref } from 'vue'
+import { PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS } from './constants'
+import { normalizePageHash } from './functions'
+
 const currentPage = ref(normalizePageHash())
-
-function normalizePageHash() {
-  const hash = window.location.hash.slice(1)
-
-  if ([PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS].includes(hash)) {
-    return hash
-  }
-
-  window.location.hash = PAGE_TIMELINE
-
-  return PAGE_TIMELINE
-}
 
 function goTo(page) {
   currentPage.value = page
--- src/pages/TheTimeline.vue
+++ src/pages/TheTimeline.vue
@@ -1,3 +1,19 @@
 <template>
-  <h1>Timeline</h1>
+  <div class="mt-7">
+    <ul>
+      <TimelineItem
+        v-for="timelineItem in timelineItems"
+        :key="timelineItem.hour"
+        :timelineItem="timelineItem"
+      />
+    </ul>
+  </div>
 </template>
+
+<script setup>
+import TimelineItem from '@/components/TimelineItem.vue'
+
+import { generateTimelineItems } from '@/functions'
+
+const timelineItems = generateTimelineItems()
+</script>
